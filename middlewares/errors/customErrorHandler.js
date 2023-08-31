beb826aacfb97f5a09c36c6571c27ad00672341c
Kubilay Uysal <kubilayuysall@gmail.com>
2023-08-31 18:27:27 +0300

    Duplicate Key Custom Message

--- middlewares/errors/customErrorHandler.js
+++ middlewares/errors/customErrorHandler.js
@@ -11,6 +11,9 @@ const customErrorHandler = (err, req, res, next) => {
     if(err.name === "ValidationError") {
         customError = new CustomError(err.message, 400);
     };
+    if(err.code === 11000) {
+        customError = new CustomError("Duplicate Key Error", 400);
+    }
 
     res.status(customError.status || 500).json({
         success: false,
