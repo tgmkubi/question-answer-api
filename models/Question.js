b1a2cfe894621ec0a89e5274cb2a39b1605bdd72
Kubilay Uysal <kubilayuysall@gmail.com>
2023-10-13 11:50:46 +0300

    Create Question Title Slug

--- models/Question.js
+++ models/Question.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const slugify = require('slugify')
+const slugify = require('slugify');
 
 const QuestionSchema = new Schema({
     title: {
@@ -24,5 +24,24 @@ const QuestionSchema = new Schema({
         ref: "User"
     }
 });
+
+QuestionSchema.pre('save', function (next) {
+    if(!this.isModified("title")) {
+        next();
+    }
+    this.slug = this.makeSlug();
+    next();
+});
+
+QuestionSchema.methods.makeSlug = function() {
+    return slugify(this.title, {
+        replacement: '-',  // replace spaces with replacement character, defaults to `-`
+        remove: /[*+~.()'"!:@]/g, // remove characters that match regex, defaults to `undefined`
+        lower: true,      // convert to lower case, defaults to `false`
+        strict: false,     // strip special characters except replacement, defaults to `false`
+        locale: 'vi',      // language code of the locale to use
+        trim: true         // trim leading and trailing replacement chars, defaults to `true`
+      })
+};
 
 module.exports = mongoose.model('Question', QuestionSchema);
