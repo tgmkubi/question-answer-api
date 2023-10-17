849d2dd6bf7cdc1441132a15c9fe9a68202a13e0
Kubilay Uysal <kubilayuysall@gmail.com>
2023-10-17 13:51:46 +0300

    Edit Question

--- routers/questions.js
+++ routers/questions.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {askNewQuestion, getAllQuestions, getSingleQuestion} = require('../controllers/question');
-const {getAccessToRoute} = require('../middlewares/authorization/auth');
+const {askNewQuestion, getAllQuestions, getSingleQuestion, editQuestion} = require('../controllers/question');
+const {getAccessToRoute, getQuestionOwnerAccess} = require('../middlewares/authorization/auth');
 const {checkQuestionExist} = require('../middlewares/database/databaseErrorHelpers');
 
 router.post('/ask', getAccessToRoute, askNewQuestion);
 router.get('/', getAllQuestions);
 router.get('/:id', checkQuestionExist, getSingleQuestion);
+router.put('/:id/edit', [getAccessToRoute, checkQuestionExist, getQuestionOwnerAccess], editQuestion);
 
 module.exports = router;
