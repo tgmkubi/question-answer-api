cac8cf7691d580c2ed2856bb0475fd998b4bcc9e
Kubilay Uysal <kubilayuysall@gmail.com>
2023-10-18 11:07:56 +0300

    Get All Answers of the Question

--- controllers/answer.js
+++ controllers/answer.js
@@ -20,4 +20,16 @@ const addNewAnswerToQuestion = asyncErrorWrapper(async (req, res, next) => {
     })
 });
 
-module.exports = {addNewAnswerToQuestion}
+const getAllAnswersByQuestion = asyncErrorWrapper(async (req, res, next) => {
+    const {question_id} = req.params;
+    const questions = await Question.findById(question_id).populate("answers");
+    const answers = questions.answers;
+
+    res.status(200).json({
+        success: true,
+        count: answers.length,
+        data: answers
+    })
+});
+
+module.exports = {addNewAnswerToQuestion, getAllAnswersByQuestion}
--- routers/answer.js
+++ routers/answer.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-const {addNewAnswerToQuestion} = require('../controllers/answer');
+const {addNewAnswerToQuestion, getAllAnswersByQuestion} = require('../controllers/answer');
 const {getAccessToRoute} = require('../middlewares/authorization/auth');
 
 router.post("/", getAccessToRoute, addNewAnswerToQuestion);
+router.get("/", getAllAnswersByQuestion);
+
 module.exports = router;
