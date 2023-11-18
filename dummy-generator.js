f5abe43573f936c1f86d8a0771b49ade34691073
Kubilay Uysal <kubilayuysall@gmail.com>
2023-11-18 16:53:36 +0300

    Dummy Generator File Update

--- dummy-generator.js
+++ dummy-generator.js
@@ -1,21 +1,19 @@
-const Question = require("./models/Question");
-const Answer = require("./models/Answer");
-const User = require("./models/User");
-const fs = require("fs");
-const connectDatabase = require("./helpers/database/connectDatabase");
-const CustomError = require("./helpers/error/CustomError");
+const User = require('./models/User');
+const Question = require('./models/Question');
+const Answer = require('./models/Answer');
+const fs = require('fs');
+const connectDatabase = require('./helpers/database/connectDatabase');
+const CustomError = require('./helpers/error/CustomError');
+const dotenv = require('dotenv');
 
-const dotenv = require("dotenv");
+const path = './dummy/';
 
-const path = "./dummy/";
-
-const users = JSON.parse(fs.readFileSync(path + "users.json" ));
-const questions = JSON.parse(fs.readFileSync(path + "questions.json" ));
-const answers = JSON.parse(fs.readFileSync(path + "answers.json" ));
-
+const users = JSON.parse(fs.readFileSync(path + 'users.json'));
+const questions = JSON.parse(fs.readFileSync(path + 'questions.json'));
+const answers = JSON.parse(fs.readFileSync(path + 'answers.json'));
 
 dotenv.config({
-    path : "./config/env/config.env"
+    path: './config/env/config.env'
 });
 
 connectDatabase();
